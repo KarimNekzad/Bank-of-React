7b92bfb09eceda4f43b6d995e6f7e069178f7eac
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-16 17:03:18 -0500

    adding debit works

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -8,7 +8,16 @@ class Debits extends Component {
     super(props)
     this.state = {
       data: [],
+      temp: {
+        amount: "",
+        date: "",
+        description: "",
+        id: "",
+      },
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.submitForm = this.submitForm.bind(this)
   }
 
   async componentDidMount() {
@@ -22,10 +31,52 @@ class Debits extends Component {
     this.setState({ data: response.data })
   }
 
+  handleChange(e) {
+    e.preventDefault()
+    this.setState({
+      temp: {
+        ...this.state.temp,
+        [e.target.name]: e.target.value,
+      },
+    })
+  }
+
+  submitForm() {
+    let updatedData = [...this.state.data]
+    updatedData.push(this.state.temp)
+    this.setState({ data: updatedData })
+  }
+
   render() {
     console.log("render debits")
     return (
       <div>
+        <h3>Add Debit</h3>
+        <form onSubmit={this.handleChange}>
+          <div>
+            <label htmlFor="">Amount</label>
+            <input onChange={this.handleChange} type="text" name="amount" />
+          </div>
+          <div>
+            <label htmlFor="">Date</label>
+            <input onChange={this.handleChange} type="text" name="date" />
+          </div>
+          <div>
+            <label htmlFor="">Description</label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="description"
+            />
+          </div>
+          <div>
+            <label htmlFor="">Id</label>
+            <input onChange={this.handleChange} type="text" name="id" />
+          </div>
+
+          <button onClick={this.submitForm}>Submit</button>
+        </form>
+
         <h1>Debits</h1>
         {this.state.data.map((data, index) => (
           <DisplayApiDebit
