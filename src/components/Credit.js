ab970d1842e3cb9f1abef66f311a1d2ea4c53a9c
dkamalov <dkamalov8@gmail.com>
2021-01-16 17:23:24 -0500

    Credit Add
    added form functionality

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -7,9 +7,17 @@ class Credit extends Component {
     constructor(props){
         super(props);
         this.state = {
-            creditData: []
+            creditData: [],
+            temp: {
+              amount: "",
+              date: "",
+              description: "",
+              id: "",
+            },
         }
-    };
+    this.handleChange = this.handleChange.bind(this)
+    this.submitForm = this.submitForm.bind(this)
+    }
 
   async componentDidMount() {
     console.log("comp did mount credit");
@@ -21,12 +29,54 @@ class Credit extends Component {
     this.setState({ creditData: response.data });
   }
 
+  submitForm() {
+    let updatedData = [...this.state.creditData]
+    updatedData.push(this.state.temp)
+    this.setState({ creditData: updatedData })
+  }
+
+  handleChange(e) {
+    e.preventDefault()
+    this.setState({
+      temp: {
+        ...this.state.temp,
+        [e.target.name]: e.target.value,
+      },
+    })
+  }
+
+
   render() {
     console.log("render credit")
     return (
-
       <div>
-        <p>Credit</p>
+
+        <h3> Add Credit Transaction</h3>
+        <form onSubmit={this.handleChange}>
+
+        <div>
+          <label htmlForm="">Amount</label>
+          <input onChange = {this.handleChange} type="text" name="amount"/>
+        </div>
+        <div>
+          <label htmlFor="">Date</label>
+          <input onChange={this.handleChange} type="text" name="date" />
+        </div>
+        <div>
+        <label htmlFor="">Description</label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="description"
+            />
+        </div>
+          <label htmlFor="">ID</label>
+          <input onChange={this.handleChange} type="text" name="id"/>
+
+          <button onClick={this.submitForm}>Submit</button>
+      </form>
+
+        <h1>Credit</h1>
         {this.state.creditData.map((creditData, index) => (
 
         <DisplayApiDebit
