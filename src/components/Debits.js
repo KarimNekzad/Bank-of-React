63c1add7b23f2b11d914c506a41ebad9507c8f54
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-16 16:12:21 -0500

    displaying debits api working

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react"
 import axios from "axios"
 
+import DisplayApiDebit from "./DisplayApiDebit"
+
 class Debits extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+    }
+  }
+
   async componentDidMount() {
     console.log("comp did mount debits")
     await this.fetchDebits()
@@ -9,13 +18,24 @@ class Debits extends Component {
 
   async fetchDebits() {
     const response = await axios.get("https://moj-api.herokuapp.com/debits")
+    console.log("response", response.data)
+    this.setState({ data: response.data })
   }
 
   render() {
     console.log("render debits")
     return (
       <div>
-        <p>Debits</p>
+        <h1>Debits</h1>
+        {this.state.data.map((data, index) => (
+          <DisplayApiDebit
+            key={index}
+            amount={data.amount}
+            date={data.date}
+            description={data.description}
+            id={data.id}
+          />
+        ))}
       </div>
     )
   }
